89e2fb358150f41006eb7a77780238ace07e2a83
Cheton Wu <cheton@gmail.com>
2018-01-03 17:00:21 +0800

    Setup tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import { test } from 'tap';
 import '../setupTests';
+import Repeatable from '../src';
 
-test('noop', (t) => {
+test('<Repeatable />', (t) => {
+    const wrapper = mount((
+        <Repeatable>
+            <button type="button"></button>
+        </Repeatable>
+    ));
+    t.equal(wrapper.find(Repeatable).length, 1, 'should render <Repeatable /> component');
+    t.end();
+});
+
+test('simulates click event', (t) => {
+    const onClick = sinon.spy();
+    const wrapper = mount(<Repeatable onClick={onClick} />);
+    wrapper.find(Repeatable).simulate('click');
+    t.ok(onClick.calledOnce, 'should be called once');
     t.end();
 });
